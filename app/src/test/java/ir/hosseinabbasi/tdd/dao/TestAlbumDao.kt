54a265d41ead30683393e3a90ef07f939dc4512a
Hossein <hossein@safeboda.com>
2019-02-24 13:08:35 +0330

    Create AlbumDaoReal

--- app/src/test/java/ir/hosseinabbasi/tdd/dao/TestAlbumDao.kt
+++ app/src/test/java/ir/hosseinabbasi/tdd/dao/TestAlbumDao.kt
@@ -8,12 +8,14 @@ class TestAlbumDao {
 
     // define a variable for the Dao we are testing.
     lateinit var albumDao: AlbumDao
+    lateinit var albumDaoReal: AlbumDao
 
     @Before
     fun setup() {
 
         albumDao = AlbumDaoStubImpl()
         println("Before: running init before EACH tests.")
+        albumDaoReal = AlbumDaoImpl()
     }
 
     @Test
@@ -32,6 +34,17 @@ class TestAlbumDao {
 
         val albums = albumDao.getAlbums("1").map { it[0] }
         val albumMocked = AlbumDto(1, 10, "albumNumber1")
+
+        albums.test().assertResult(albumMocked)
+
+        println("TEST: Running Album1ShouldReturnResult test.")
+    }
+
+    @Test
+    fun `search for first album of user number 1 should return exact result from real dao`() {
+
+        val albums = albumDaoReal.getAlbums("1").map { it[1] }
+        val albumMocked = AlbumDto(2, 1, "sunt qui excepturi placeat culpa")
 
         albums.test().assertResult(albumMocked)
 
